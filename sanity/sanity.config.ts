022f89419895c66fb40ef8cf3713c9735c65a4cd
Hariram <hariram6674@gmail.com>
2024-07-06 20:53:22 +0400

    changesK

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -3,15 +3,12 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
-console.log('Sanity Project ID:', process.env.SANITY_STUDIO_PROJECT_ID)
-console.log('Sanity Dataset:', process.env.SANITY_STUDIO_DATASET)
-
 export default defineConfig({
   name: 'default',
   title: 'nextjs blog',
 
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-  dataset: process.env.SANITY_STUDIO_DATASET,
+  projectId: 'ogkhtjff',
+  dataset: 'production',
 
   plugins: [deskTool(), visionTool()],
 
